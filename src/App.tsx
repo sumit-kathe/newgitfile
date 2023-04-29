dbd6f2655fb40383f7472add0b3c4cc9ed20e8b8
sumit-kathe <sumitofficial2022@gmail.com>
2023-04-29 12:09:09 +0530

    learning how git works

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const data: number = 99999999;
+
+  const op: number = data + 1;
+
+  let st: string = "addition";
+
+  var stop: string = st + op;
+
+  const store:  number[] = [1, 2, 3, 4, 5, 6, 7];
+
+  store[0]=56;
+  console.log(store);
+  const value: string[] = ["success0", "success1", "success2", "success3"];
+
+  const material: {
+    product?: string;
+    quantity: number;
+    batch?: number;
+    MFG: string;
+  } = {
+    product: "parle -G",
+    quantity: 5,
+
+    MFG: "25 jan",
+  };
+
+  const bt = value.map((item) => {
+    return (
+      <>
+        <button>{item}</button>
+        <br></br>
+        <br></br>
+        <h1>{material.product}</h1>
+      </>
+    );
+  });
+
+ function addition(a:number,b:number):void{
+
+
+  return(console.log(a+b));
+
+
+ }
+ addition(5,8);
+
+
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {op}
+      <br></br>
+
+      {stop}
+      <br></br>
+
+      {bt}
+    </>
   );
 }
 
